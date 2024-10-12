767807d9c5029278b1e6f09d0df94a9b06d1db98
Roberto Castro <tonocastrom@gmail.com>
2024-10-12 16:04:41 -0600

    Proyecto de Arbol Sintactico

---
+++ ArbolSintactico/src/arbolsintactico/ArbolSintactico.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arbolsintactico;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.util.Scanner;
+
+/**
+ *
+ * @author roberto
+ */
+public class ArbolSintactico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Inrese un numero binario: ");
+        String entrada = consola.next();
+        
+        AnalisisLexico lexico = new AnalisisLexico(new BufferedReader(new StringReader(entrada)));
+        
+        AnalisisSintactico sintactico = new AnalisisSintactico(lexico);
+        
+        try {
+            sintactico.parse();
+            graficar(sintactico.codigo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void graficar(String codigo) {
+        String texto = """
+                       digraph G(
+                       """;
+        texto += codigo + "\n";
+        try {
+            escribirArchivo("archivo.dot",texto);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void escribirArchivo(String ruta, String texto) {
+        FileWriter fw;
+        PrintWriter pw;
+        
+        try {
+            fw = new FileWriter(ruta);
+            pw = new PrintWriter(fw);
+            pw.write(texto);
+            pw.close();
+            fw.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ ArbolSintactico/src/arbolsintactico/lexico.jflex
@@ -0,0 +1,37 @@
+package arbolsintactico;
+
+import java_cup.runtime.Symbol;
+
+%%
+%cupsym Simbolo
+%cup
+%class AnalisisLexico
+%public
+%unicode
+%line
+%char
+%ignorecase
+
+%{
+    //VA IR CODIGO EN JAVA
+    private void error(String message) {
+        //System.out.println("Error en linea "+(yline+1)+",columna "+(yycolumn+1)+" caracter: "+message);
+        System.out.println("Error: "+message);
+    }
+%}
+
+%%
+
+<YYINITIAL>{
+    "0" { 
+        return new Symbol(Simbolo.cero,yytext());
+    }
+    "1" { 
+        return new Symbol(Simbolo.uno,yytext());
+    }
+    
+    . { 
+        error(yytext());
+    }
+}
+
